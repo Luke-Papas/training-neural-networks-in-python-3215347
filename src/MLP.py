e96a6669c957135f8b2512b9a0b75148bef8b250
Luke-Papas <loucaspapaspyrou@gmail.com>
2025-01-25 15:54:04 +0000

    First NN using Sigmoid and weights

--- src/MLP.py
+++ src/MLP.py
@@ -8,9 +8,15 @@ class Perceptron:
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        pass
+        self.weights = (np.random.rand(inputs+1) *2) -1
+        #Number of Weights = # of inputs +1 for the bias
+        #Add scaling factor of 2 and a shift of -1
+        #Why?
+        self.bias = bias
+        #saving the bias term for later
 
-    def run(self, x):
+    def run(self, x): # feeds the input array x -> into the perceptron to -> return the activation functions output
         """Run the perceptron. x is a python list with the input values."""
-        pass
+        x_sum = np.dot(np.append(x,self.bias),self.weights) # dot product A*B = a1b1+a2b2+a3b3
+        return self.sigmoid(x_sum)
         
